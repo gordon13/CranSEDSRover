d648ddefb12c45b973e3d4a67fa59d2b32599a1a
Paul <paullehenaff@msn.com>
2018-06-08 16:32:29 +0100

    - more work on motor acceleration

--- src/ArduinoFirmware/RoverFirmware/Misc/acceleration_curve.py
+++ src/ArduinoFirmware/RoverFirmware/Misc/acceleration_curve.py
@@ -1,50 +1,44 @@
 import matplotlib.pyplot as plt
-import math
-
 import lut
 
 # variables
 total_iterations = 500
-offset = 0
+offset = 113
 currentspeed = 80
 targetspeed = 200
 maxspeed = 255
-accelerationtime = 2
+accelerationtime = 5
 dt = 0.1
-print("dt ", dt)
+print("length of simulation [s] ", dt * total_iterations)
+print("dt [s]", dt)
+print("acceleration iterations [iterations]", accelerationtime / dt)
+
 # lut
 sigmoid = lut.sigmoid
-number_samples = len(sigmoid)
-adt = accelerationtime / number_samples  # acceleration time interval
+number_lut_points = len(sigmoid)
+sigmoid_sample_interval = accelerationtime / number_lut_points  # acceleration time interval
 max_number_of_samples_possible_in_time = int(accelerationtime / dt)
-sample_every = int(number_samples / max_number_of_samples_possible_in_time)
-print("adt ", adt)
+sample_every = int(number_lut_points / max_number_of_samples_possible_in_time)
+print("sigmoid_sample_interval ", sigmoid_sample_interval)
 print("max samples possible ", max_number_of_samples_possible_in_time)
 print("sample_every ", sample_every)
 
-
-def normalise():
-    pass
-
-
+# main code
 points = []
 speed_difference = targetspeed - currentspeed
-t = 0
+newspeed = currentspeed
+sigmoid_i = 0
 for i in range(total_iterations):
-    if (i + offset < len(sigmoid)):
-        if (i % sample_every == 0):
-            newspeed = currentspeed + speed_difference * sigmoid[i]
+    newspeed = currentspeed + speed_difference * sigmoid[sigmoid_i]
+    # if (i % sample_every == 0):
+    if (sigmoid_i < len(sigmoid)):
+        if (i % max_number_of_samples_possible_in_time == 0):
+            sigmoid_i += sample_every
+        sigmoid_i = max(0, min(sigmoid_i, len(sigmoid) - 1))
     points.append((i, newspeed))
-    # print(i)
-    t += dt
 
 
 t = range(total_iterations)
 speed = [p[1] for p in points]
 plt.plot(t, speed)
 plt.show()
-
-
-0.1, 0.02
-0.2, 0.02
-0.3, 0.02
