af4a83e9d29c415fec5952a572c26a180e8d1151
gordon13 <paullehenaff@msn.com>
2018-06-09 04:13:36 +0100

    - Got acceleration algorithm working

--- src/ArduinoFirmware/RoverFirmware/Misc/acceleration_curve.py
+++ src/ArduinoFirmware/RoverFirmware/Misc/acceleration_curve.py
@@ -3,25 +3,31 @@ import lut
 
 # variables
 total_iterations = 500
-offset = 113
-currentspeed = 80
-targetspeed = 200
-maxspeed = 255
-accelerationtime = 5
-dt = 0.1
-print("length of simulation [s] ", dt * total_iterations)
-print("dt [s]", dt)
-print("acceleration iterations [iterations]", accelerationtime / dt)
+currentspeed = 80  # digital value
+targetspeed = 200  # digital value
+maxspeed = 255  # digital value
+dt = 0.1  # s
+totaltime = total_iterations * dt  # s
+accelerationtime = 5  # s
+acceleration_iterations = int(accelerationtime / dt)
+
+print("total time", totaltime)
+print("acceleration time", accelerationtime)
+print("")
+print("total iterations", total_iterations)
+print("acceleration_iterations", acceleration_iterations)
+print("")
+
 
 # lut
 sigmoid = lut.sigmoid
 number_lut_points = len(sigmoid)
-sigmoid_sample_interval = accelerationtime / number_lut_points  # acceleration time interval
-max_number_of_samples_possible_in_time = int(accelerationtime / dt)
-sample_every = int(number_lut_points / max_number_of_samples_possible_in_time)
-print("sigmoid_sample_interval ", sigmoid_sample_interval)
-print("max samples possible ", max_number_of_samples_possible_in_time)
-print("sample_every ", sample_every)
+sample_every = int(number_lut_points / acceleration_iterations)
+
+print("sigmoid points", number_lut_points)
+print("sample every", sample_every)
+
+
 
 # main code
 points = []
@@ -30,15 +36,15 @@ newspeed = currentspeed
 sigmoid_i = 0
 for i in range(total_iterations):
     newspeed = currentspeed + speed_difference * sigmoid[sigmoid_i]
-    # if (i % sample_every == 0):
-    if (sigmoid_i < len(sigmoid)):
-        if (i % max_number_of_samples_possible_in_time == 0):
-            sigmoid_i += sample_every
-        sigmoid_i = max(0, min(sigmoid_i, len(sigmoid) - 1))
+    if (sigmoid_i + sample_every < len(sigmoid)):
+        sigmoid_i += sample_every
     points.append((i, newspeed))
 
 
 t = range(total_iterations)
 speed = [p[1] for p in points]
 plt.plot(t, speed)
+plt.ylim(0, maxspeed)
+plt.ylabel("Speed value")
+plt.xlabel("Iterations")
 plt.show()
